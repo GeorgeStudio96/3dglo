ed165db1bfa909f5f723a86e83c451c9dfc3bb61
George Ershov <geo.erh96@gmail.com>
2023-02-06 17:44:54 +0300

    исправлено на match media

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -3,40 +3,33 @@ const modal = () => {
     const popup = document.querySelector('.popup')
     const popupClose = popup.querySelector('.popup-close')
 
-    window.addEventListener('resize', () => {
-        const width = document.documentElement.clientWidth
-        if (width > 768) {
-            popup.style.opacity = 0
 
-            const modalAnimationOpen = () => {
-                popup.style.opacity = 1;
-                popup.style.transition = "opacity 1s";
-            }
-            const modalAnimationClose = () => {
-                popup.style.opacity = 0;
-                popup.style.transition = "opacity 1s";
-            }
-            const popupHide = () => {
-                popup.style.display = 'none'
-            }
+    popup.style.opacity = 0
 
-            popupBtn.forEach(item => {
-                item.addEventListener('click', () => {
-                    popup.style.display = 'block'
-                    setTimeout(modalAnimationOpen, 100)
+    const modalAnimationOpen = () => {
+        popup.style.opacity = 1;
+        popup.style.transition = "opacity 1s";
+    }
+    const modalAnimationClose = () => {
+        popup.style.opacity = 0;
+        popup.style.transition = "opacity 1s";
+    }
+    const popupHide = () => {
+        popup.style.display = 'none'
+    }
 
-                })
+    if (window.matchMedia("(min-width: 768px)").matches) {
+        popupBtn.forEach(item => {
+            item.addEventListener('click', () => {
+                popup.style.display = 'block'
+                setTimeout(modalAnimationOpen, 100)
             })
-            popupClose.addEventListener('click', () => {
-                modalAnimationClose()
-                setTimeout(popupHide, 1000)
-            })
-        }
-    })
-
-
-
-
+        })
+        popupClose.addEventListener('click', () => {
+            modalAnimationClose()
+            setTimeout(popupHide, 1000)
+        })
+    }
 }
 
 export default modal
