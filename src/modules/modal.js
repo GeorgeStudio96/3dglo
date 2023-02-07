a208754b0eead81cdd97cf010f8ac8062e00d854
George Ershov <geo.erh96@gmail.com>
2023-02-07 19:38:09 +0300

    resize

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,9 +2,10 @@ const modal = () => {
     const popupBtn = document.querySelectorAll('.popup-btn')
     const popup = document.querySelector('.popup')
     const popupClose = popup.querySelector('.popup-close')
+    const width = document.documentElement.clientWidth
 
 
-    popup.style.opacity = 0
+
 
     const modalAnimationOpen = () => {
         popup.style.opacity = 1;
@@ -18,18 +19,45 @@ const modal = () => {
         popup.style.display = 'none'
     }
 
-    if (window.matchMedia("(min-width: 768px)").matches) {
-        popupBtn.forEach(item => {
-            item.addEventListener('click', () => {
-                popup.style.display = 'block'
-                setTimeout(modalAnimationOpen, 100)
+
+
+
+
+    window.addEventListener('resize', () => {
+
+        if (width > 576) {
+
+            popup.style.opacity = 0
+
+            popupBtn.forEach(item => {
+                item.addEventListener('click', () => {
+                    popup.style.display = 'block'
+                    setTimeout(modalAnimationOpen, 100)
+                })
             })
-        })
-        popupClose.addEventListener('click', () => {
-            modalAnimationClose()
-            setTimeout(popupHide, 1000)
-        })
-    }
+
+            popupClose.addEventListener('click', () => {
+                modalAnimationClose()
+                setTimeout(popupHide, 1000)
+            })
+
+        } else {
+            console.log('меньше 576');
+
+            popupBtn.forEach(item => {
+                item.addEventListener('click', () => {
+                    popup.style.display = 'block'
+                })
+            })
+
+            popupClose.addEventListener('click', () => {
+                popup.style.display = 'none'
+            })
+
+        }
+
+    })
+
 }
 
 export default modal
