d07f8c91339bae69c7893675be0ec678edf09833
George Ershov <geo.erh96@gmail.com>
2023-02-10 10:27:46 +0300

    fix modal animation

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,72 +1,35 @@
+import animate from "./animate"
+
 const modal = () => {
+
     const popupBtn = document.querySelectorAll('.popup-btn')
     const popup = document.querySelector('.popup')
     const popupClose = popup.querySelector('.popup-close')
-    const width = document.documentElement.clientWidth
 
-
-
-
-    const modalAnimationOpen = () => {
-        popup.style.opacity = 1;
-        popup.style.transition = "opacity 1s";
-    }
-    const modalAnimationClose = () => {
-        popup.style.opacity = 0;
-        popup.style.transition = "opacity 1s";
-    }
-    const popupHide = () => {
+    popupClose.addEventListener('click', () => {
         popup.style.display = 'none'
-    }
-
-
-    const clientWidthMax = () => {
-        popup.style.opacity = 0
-
-        popupBtn.forEach(item => {
-            item.addEventListener('click', () => {
-                popup.style.display = 'block'
-                setTimeout(modalAnimationOpen, 100)
-            })
-        })
-
-        popupClose.addEventListener('click', () => {
-            modalAnimationClose()
-            setTimeout(popupHide, 1000)
-        })
-    }
-
-    const clientWidthMin = () => {
-        console.log('меньше 576');
-
-        popupBtn.forEach(item => {
-            item.addEventListener('click', () => {
-                popup.style.display = 'block'
-            })
-        })
-
-        popupClose.addEventListener('click', () => {
-            popup.style.display = 'none'
-        })
-    }
-
-
-
-    if (width > 576) {
-        clientWidthMax()
-    } else {
-        clientWidthMin()
-    }
-
-    window.addEventListener('resize', () => {
-
-        if (width > 576) {
-            clientWidthMax()
-        } else {
-            clientWidthMin()
-        }
+        popup.style.opacity = '0';
     })
 
+    popupBtn.forEach(item => {
+        item.addEventListener('click', () => {
+            if (window.innerWidth < 700) {
+                popup.style.display = 'block'
+                popup.style.opacity = '1';
+            } else {
+                popup.style.display = 'block'
+                animate({
+                    duration: 1000,
+                    timing: function (timeFraction) {
+                        return timeFraction;
+                    },
+                    draw: function (progress) {
+                        popup.style.opacity = progress;
+                    }
+                })
+            }
+        })
+    })
 }
 
 export default modal
