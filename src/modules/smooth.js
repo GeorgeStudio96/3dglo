eb9c24d0294c6cc6d72b4c9932b9a21c984973b5
George Ershov <geo.erh96@gmail.com>
2023-02-10 10:49:55 +0300

    menu fixed bug

--- src/modules/smooth.js
+++ src/modules/smooth.js
@@ -1,19 +1,27 @@
 const smooth = () => {
     const anchors = document.querySelectorAll('a[href*="#"]')
 
+    const search = 'close';
+    const elem = Array.from(document.querySelectorAll('a')).find(el => el.href.includes(search));
 
-    anchors.forEach(item => {
-        item.addEventListener('click', (e) => {
-            e.preventDefault();
+    console.log(elem);
 
-            const blockID = item.getAttribute('href').substr(1)
 
-            document.getElementById(blockID).scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
+    if (!elem) {
+        anchors.forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                const blockID = item.getAttribute('href').substr(1)
+
+                document.getElementById(blockID).scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                })
             })
         })
-    })
+    }
+
 }
 
 
